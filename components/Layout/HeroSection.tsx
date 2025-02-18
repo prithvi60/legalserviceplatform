3d1233be99be36469e38563653a97147f4af6ce4
Prithvi <prithvin19@gmail.com>
2025-02-18 16:57:51 +0530

    ani wip 1

--- components/Layout/HeroSection.tsx
+++ components/Layout/HeroSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { FaStar } from "react-icons/fa6";
+// import { FaStar } from "react-icons/fa6";
 import OurClients from "./OurClients";
 import { motion } from "framer-motion";
 
